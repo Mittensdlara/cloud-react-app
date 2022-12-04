58db5bac1f422a852827f7d8b051acdff85dd62b
Mittensdlara <pooraghaeedelara@gmail.com>
2022-12-04 17:31:12 +0330

    gitignore Added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+import About from './components/About';
+import Support from './components/Support';
+import Pricing from './components/Pricing';
+import Platforms from './components/Platforms';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <Pricing />
+      <Platforms />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
